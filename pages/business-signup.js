ddf234c0647d51942825d59a684dd6dbff273141
Joel-mq <joel.d.woodhouse@gmail.com>
2024-08-15 20:00:29 +1000

    additions to BusinessSignupPage
    added
    - registration password field
    - email submit button
    - email signup button
    
    - input email function
    - input password function

--- pages/business-signup.js
+++ pages/business-signup.js
@@ -1,12 +1,31 @@
 export class BusinessSignupPage {
     constructor(page) {
         this.page = page;
+  
         this.registrationEmailInputField = page.getByTestId('registration-email');
-        this
+        this.registrationSubmitEmailButton = page.getByTestId('submit-button');
+
+        this.registrationPasswordInputField = page.getByTestId('registration-password');
+        this.registrationSignupButton = page.getByTestId('email-sign-up');
     }
 
     async isRegistrationEmailInputFieldVisible() {
         const registrationEmailInputField = this.registrationEmailInputField;
         return await registrationEmailInputField.isVisible();
+    }
+
+    async isRegistrationPasswordInputFieldVisible() {
+        const registrationPasswordInputField = this.registrationPasswordInputField;
+        return await registrationPasswordInputField.isVisible();
+    }
+
+    async enterRegistrationEmail(email) {
+        const registrationEmailInputField = this.registrationEmailInputField;
+        await registrationEmailInputField.fill(email);
+    }
+
+    async enterPassword(password) {
+        const registrationPasswordInputField = this.registrationPasswordInputField;
+        await registrationPasswordInputField.fill(password);
     }
 }
